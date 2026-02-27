e3d3244dcca82ee2584639f2653011b36fd635ab
Juan Jose <109545739+JuanJose505@users.noreply.github.com>
2026-02-27 07:58:09 -0500

    update

---
+++ EjerciciosPOO/EjerciciosPOO/Modelos/Ejercicio1/CuentaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPOO.Modelos.Ejercicio1
+{
+    public class CuentaBancaria
+    {
+        string Titular { get; set; }
+        decimal Saldo { get; set; }
+
+        public CuentaBancaria(string titular, decimal saldo)
+        {
+            string Titular = titular;
+            decimal Saldo = saldo;
+        }
+
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"El saldo del usuario es de {Saldo}");
+        }
+
+        public void Depositar(decimal cantidad)
+        {
+            if (cantidad >= 1)
+            {
+                Saldo += cantidad;
+                Console.WriteLine($"Deposito exitoso, el saldo actual es de {Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("La cantidad es negativa, no se puede continuar");
+            }
+
+        }
+
+        public void Retirar(decimal cantidad)
+        {
+            if (Saldo >= cantidad)
+            {
+                Saldo -= cantidad;
+                Console.WriteLine($"Retiro exitoso, saldo actual {Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("El saldo no es suficiente para continuar");
+            }
+        }
+    }
+}
---
+++ EjerciciosPOO/EjerciciosPOO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjerciciosPOO.Modelos.Ejercicio1;
+
+namespace EjerciciosPOO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-CuentaBancaria");
+            int menu = int.Parse(Console.ReadLine());
+            if (menu == 1)
+            {
+                CuentaBancaria cuenta = new CuentaBancaria("Alvaro", 100000);
+                cuenta.ConsultarSaldo();
+                cuenta.Depositar(30000);
+                cuenta.Retirar(30000);
+            }
+        }
+    }
+}
