4e8f4b590ca14d1856f6b35a81e43827925a008a
Juan Jose <109545739+JuanJose505@users.noreply.github.com>
2026-04-14 13:35:09 -0500

    Control de ventas diarias added

--- Segundo Corte/Control de Ventas Diarias/Control de Ventas Diarias/Form1.Designer.cs
+++ Segundo Corte/Control de Ventas Diarias/Control de Ventas Diarias/Form1.Designer.cs
@@ -28,13 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numMonto = new System.Windows.Forms.NumericUpDown();
+            this.btnGuardarTransaccion = new System.Windows.Forms.Button();
+            this.lbHistorial = new System.Windows.Forms.ListBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGuardarTransaccion);
+            this.groupBox1.Controls.Add(this.numMonto);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(466, 196);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nueva Venta";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCargar);
+            this.groupBox2.Controls.Add(this.lblTotal);
+            this.groupBox2.Controls.Add(this.lbHistorial);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 214);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox2.Size = new System.Drawing.Size(466, 208);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Historial";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Transaccion";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(19, 57);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(204, 22);
+            this.txtID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Monto ($)";
+            // 
+            // numMonto
+            // 
+            this.numMonto.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numMonto.Location = new System.Drawing.Point(232, 58);
+            this.numMonto.Maximum = new decimal(new int[] {
+            -1593835520,
+            466537709,
+            54210,
+            0});
+            this.numMonto.Name = "numMonto";
+            this.numMonto.Size = new System.Drawing.Size(228, 22);
+            this.numMonto.TabIndex = 3;
+            // 
+            // btnGuardarTransaccion
+            // 
+            this.btnGuardarTransaccion.Location = new System.Drawing.Point(19, 137);
+            this.btnGuardarTransaccion.Name = "btnGuardarTransaccion";
+            this.btnGuardarTransaccion.Size = new System.Drawing.Size(204, 39);
+            this.btnGuardarTransaccion.TabIndex = 4;
+            this.btnGuardarTransaccion.Text = "Guardar Transaccion";
+            this.btnGuardarTransaccion.UseVisualStyleBackColor = true;
+            this.btnGuardarTransaccion.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbHistorial
+            // 
+            this.lbHistorial.FormattingEnabled = true;
+            this.lbHistorial.ItemHeight = 16;
+            this.lbHistorial.Location = new System.Drawing.Point(6, 21);
+            this.lbHistorial.Name = "lbHistorial";
+            this.lbHistorial.Size = new System.Drawing.Size(454, 132);
+            this.lbHistorial.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.ForeColor = System.Drawing.Color.Blue;
+            this.lblTotal.Location = new System.Drawing.Point(314, 174);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(60, 16);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "TOTAL:";
+            this.lblTotal.Visible = false;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(6, 167);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(122, 31);
+            this.btnCargar.TabIndex = 5;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(490, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Control de ventas diarias";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonto)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnGuardarTransaccion;
+        private System.Windows.Forms.NumericUpDown numMonto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ListBox lbHistorial;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- Segundo Corte/Control de Ventas Diarias/Control de Ventas Diarias/Form1.cs
+++ Segundo Corte/Control de Ventas Diarias/Control de Ventas Diarias/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,38 @@ namespace Control_de_Ventas_Diarias
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ruta = "ventas.csv";
+            string linea = $"{txtID.Text};{numMonto.Value}";
+            File.AppendAllText(ruta, linea + Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbHistorial.Items.Clear();
+            string ruta = "ventas.csv";
+            string[] lineas = File.ReadAllLines(ruta);
+            decimal MontoTotal = 0;
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                string[] campos = lineas[i].Split(';');
+                if (campos.Length >= 2)
+                {
+                    string ID = campos[0];
+                    decimal Monto = decimal.Parse(campos[1]);
+                    MontoTotal = MontoTotal + Monto;
+
+                    string linea = $"{ID} - {Monto}";
+                    lbHistorial.Items.Add(linea);                    
+                    
+                }
+                lblTotal.Visible = true;
+                lblTotal.Text = $"TOTAL: {MontoTotal}";
+
+            }
         }
     }
 }
